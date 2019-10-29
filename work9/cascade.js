ed327d5ea23b66303407a2e7ca1839a014de1b63
1907Linjd <1914773513@qq.com>
2019-10-29 14:55:34 +0800

    work10

--- work9/cascade.js
+++ work9/cascade.js
@@ -95,4 +95,5 @@ var classSelect =document.getElementById('classSelect');
 majorSelect.onchange=function(){
     classSelect.options.length = 0;
     createOption(classSelect,classSelectArr[collegeSelect.value][majorSelect.value])
+
 };
